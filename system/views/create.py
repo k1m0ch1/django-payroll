3a4d87cb64e042315e3616aea61a03f0b2e127a5
Yahya Fadhlulloh Al-Fatih <yahya.kimochi@gmail.com>
2017-12-21 04:15:40 +0700

    remove NIK from add karyawan and remove line code disable device

--- system/views/create.py
+++ system/views/create.py
@@ -135,10 +135,10 @@ def karyawan_save(request):
 	zk = ZK(ipmesin, port=4370, timeout=5)
 	try:
 		conn = zk.connect()
-		conn.disable_device()
+		#conn.disable_device()
 		conn.set_user(uid=int(str(k.fingerid).strip()), name="" + str(k.name), privilege=const.USER_DEFAULT, password='12345678', group_id='', user_id="" + str(k.fingerid).strip() )
-		conn.test_voice()
-		conn.test_voice()
+		#conn.test_voice()
+		#conn.test_voice()
 		conn.test_voice()
 		conn.enable_device()
 	except Exception, e:
