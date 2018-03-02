ca2cfbfd0b4f596820d0e83cebd2e084b66b542f
Yahya Fadhlulloh Al-Fatih <yahya.kimochi@gmail.com>
2018-03-03 03:01:25 +0700

    change the base of resetData

--- resetData.bat
+++ resetData.bat
@@ -9,3 +9,4 @@ python manage.py migrate
 python manage.py loaddata perusahaan departemen bagian golongan jabatan bank warganegara agama statusmenikah modules
 python manage.py loaddata lokasiperusahaan karyawan shift karyawanshift pinjaman inventory absensi hariraya settings
 python manage.py loaddata gajipokok potongan masatenggangclosing postinggaji izindancuti tunjangankaryawan bpjs mesin
+python manage.py exportxls
