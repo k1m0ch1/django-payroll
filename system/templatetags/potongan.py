da00b8eee890d65e5b60e33beccdf058c3100663
Yahya Fadhlulloh Al-Fatih <yahya.kimochi@gmail.com>
2018-04-20 16:59:33 +0700

    fixing form halaman potongan

--- system/templatetags/potongan.py
+++ system/templatetags/potongan.py
@@ -4,6 +4,29 @@ from django.core.urlresolvers import reverse
 register = template.Library()
 
 @register.simple_tag
-def sisa_cicil(pinjaman=None, banyak_cicil=None):
+def banyak_cicil(pinjaman=None, banyak_cicil=None):
 	hasil = pinjaman / banyak_cicil
+	money = str(int(hasil))
+	i = 0
+	hasil = "Rp. "
+	y = len(str(money))
+	for x in str(money):
+		if y%3 == 0 and y != 1 and y != len(str(money)):
+			hasil = hasil + "."
+		hasil = hasil + x
+		y = y - 1
+	return hasil
+
+@register.simple_tag
+def sisa_cicil(pinjaman=None, banyak_cicil=None, sisa_cicil=None):
+	hasil = (pinjaman / banyak_cicil) * sisa_cicil
+	money = str(int(hasil))
+	i = 0
+	hasil = "Rp. "
+	y = len(str(money))
+	for x in str(money):
+		if y%3 == 0 and y != 1 and y != len(str(money)):
+			hasil = hasil + "."
+		hasil = hasil + x
+		y = y - 1
 	return hasil
